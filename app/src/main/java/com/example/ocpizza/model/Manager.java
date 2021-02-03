826cd114bc4affb20155dec10cd564a393b744a8
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2021-02-03 15:47:14 +0100

    Setting MainActivity layout.

--- app/src/main/java/com/example/ocpizza/model/Manager.java
+++ app/src/main/java/com/example/ocpizza/model/Manager.java
@@ -3,7 +3,6 @@ package com.example.ocpizza.model;
 import com.example.ocpizza.util.Constants;
 
 public class Manager extends Employee{
-
     public Manager() {
         setPosition(Constants.MANAGER_TEXT);
     }
