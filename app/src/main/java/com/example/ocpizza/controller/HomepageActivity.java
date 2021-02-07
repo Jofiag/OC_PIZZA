b45a9e43675fe21246142a7d76bcdb8201f61da8
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2021-02-07 08:12:20 +0100

    Begin implementation of the searching widget.

--- app/src/main/java/com/example/ocpizza/controller/HomepageActivity.java
+++ app/src/main/java/com/example/ocpizza/controller/HomepageActivity.java
@@ -1,7 +1,13 @@
 package com.example.ocpizza.controller;
 
 import android.annotation.SuppressLint;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,8 +22,10 @@ import com.example.ocpizza.R;
 import com.example.ocpizza.fragment.RestaurantListViewFragment;
 import com.example.ocpizza.fragment.RestaurantMapViewFragment;
 import com.example.ocpizza.fragment.WorkmateListViewFragment;
+import com.example.ocpizza.util.Constants;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -25,6 +33,7 @@ public class HomepageActivity extends AppCompatActivity {
     private Toolbar myToolbar;
     private DrawerLayout myDrawerLayout;
     private NavigationView myNavigationView;
+    private MaterialSearchView materialSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +41,18 @@ public class HomepageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
 
         setReferences();
+
         attachNewFragment(new RestaurantMapViewFragment()); // Showing the RestaurantMapViewFragment when first start the activity
+
         setBottomNavigationView();
+
         setMyToolbarAsAppBar();
+
         setMyDrawerLayout();
+
         setMyNavigationView();
+
+        setMaterialSearchView();
     }
 
     private void setReferences() {
@@ -44,6 +60,7 @@ public class HomepageActivity extends AppCompatActivity {
         myToolbar = findViewById(R.id.my_toolbar);
         myDrawerLayout = findViewById(R.id.my_drawer);
         myNavigationView = findViewById(R.id.my_navigation_view);
+        materialSearchView = findViewById(R.id.my_material_search_view);
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -53,12 +70,15 @@ public class HomepageActivity extends AppCompatActivity {
 
             switch (id){
                 case R.id.restaurant_map_view_item:
+                    myToolbar.setTitle(Constants.IM_HUNGRY_TITLE_TEXT);
                     attachNewFragment(new RestaurantMapViewFragment());
                     return true;
                 case R.id.restaurant_list_view_item:
+                    myToolbar.setTitle(Constants.IM_HUNGRY_TITLE_TEXT);
                     attachNewFragment(new RestaurantListViewFragment());
                     return true;
                 case R.id.workmate_list_view_item:
+                    myToolbar.setTitle(Constants.AVAILABLE_WORKMATES_TITLE_TEXT);
                     attachNewFragment(new WorkmateListViewFragment());
                     return true;
             }
@@ -116,12 +136,60 @@ public class HomepageActivity extends AppCompatActivity {
         });
     }
 
-    //Make sure we close the DrawerLayout when the user click on back button
+
     @Override
+    //Make sure we close the DrawerLayout when the user click on back button
     public void onBackPressed() {
         if (myDrawerLayout.isOpen())
             myDrawerLayout.closeDrawers();
         else
             super.onBackPressed();
+    }
+
+    private String getRestaurantNameSearchedByUser(){
+        String restaurantName = "";
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction()))
+            restaurantName = intent.getStringExtra(SearchManager.QUERY);
+
+        return restaurantName;
+    }
+
+    private void doMyResearch(String restaurantName){
+
+    }
+
+    private void setMaterialSearchView(){
+        String[] array = new String[]{"Hello", "How", "Are", "You", "Today"};
+
+        //materialSearchView.closeSearch();
+        materialSearchView.setSuggestions(array);
+        materialSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //search the query that the user enter to search
+                materialSearchView.setVisibility(View.VISIBLE);
+                doMyResearch(getRestaurantNameSearchedByUser());
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Make changes in realtime
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_view_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.search_item);
+        materialSearchView.setMenuItem(item);
+
+        return true;
     }
 }
--- app/src/main/java/com/example/ocpizza/util/Constants.java
+++ app/src/main/java/com/example/ocpizza/util/Constants.java
@@ -4,8 +4,10 @@ public class Constants {
     public static final String MANAGER_TEXT = "Manager";
     public static final String PIZZAIOLO_TEXT = "Pizzaiolo";
     public static final String DELIVERY_MAN_TEXT = "Delivery man";
+    public static final String IM_HUNGRY_TITLE_TEXT = "I'm hungry!";
     public static final String COOKING_COMPLETED_TEXT = "Cooking completed";
     public static final String SHIPPING_COMPLETED_TEXT = "Shipping completed";
     public static final String COOKING_IN_PROGRESS_TEXT = "Cooking in progress";
     public static final String SHIPPING_IN_PROGRESS_TEXT = "Shipping in progress";
+    public static final String AVAILABLE_WORKMATES_TITLE_TEXT = "Available workmates";
 }
